1f92ce16317a6914a92956c01a91b2041ed4abcd
wizardy <wizardhassan99@gmail.com>
2023-09-24 11:46:31 +0100

    sorry boolean haha

--- basics/data-types/DataTypes.java
+++ basics/data-types/DataTypes.java
@@ -4,7 +4,7 @@ public class DataTypes {
     int lucky_number = 7;               // Integer (whole number)
     float floatingNumber = 19.99f;    // Floating point number
     char lonelyLetter = 'O';         // Character
-    boolean FuckingBoolean = true;       // Boolean
+    boolean honestBoolean = true;       // Boolean
     String badJoke = "Just laugh u don't need it"; // String
     System.out.println(badJoke);
   }
