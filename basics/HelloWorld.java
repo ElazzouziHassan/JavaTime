e0b5f371d67fead71fd803e04bf227beb63eca0a
wizardy <wizardhassan99@gmail.com>
2023-09-24 10:51:32 +0100

    bases numériques

--- basics/HelloWorld.java
+++ basics/HelloWorld.java
@@ -18,6 +18,7 @@ public class HelloWorld {
  *  - javac HelloWorld.java -> Build
  *  - java HelloWorld -> Compile
  *  - output : Hello, World! 
+ * ------------------------------------------------------------
  * 3 .Types de donneés :
  *  + boolean : Boolean, 1 octet, true(1) | false(0)
  *  + byte    : Byte, 1 octet, -128 à 127
@@ -27,4 +28,11 @@ public class HelloWorld {
  *  + long    : Long, 8 octets, -2^63 à (2^63)-1
  *  + float   : Float, 4 octets, 1.4x(10^-45) à 3.4x(10^38)
  *  + double  : Double, 8 octets, 4.9x(10^-324) à 1.7x(10^308)
+ * ------------------------------------------------------------
+ * 4 .Bases numériques :
+ * Décimale    : 10 -> 0 à 9 : normal
+ * Binaire     : 2  -> 0 et 1: 0bxxxx
+ * Octale      : 8  -> 0 à 7 : 07xxxx
+ * Hexadécimale: 16 -> 0 è F : 0Xxxxx
+ * ------------------------------------------------------------
  */
