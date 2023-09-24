061dbc3e30e1136a92edd672e916730d0714942e
wizardy <wizardhassan99@gmail.com>
2023-09-24 11:24:52 +0100

    commit

---
+++ basics/variables/Variables.java
@@ -0,0 +1,9 @@
+package variables;
+
+public class Variables {
+  public static void main(String[] args) {
+    // Syntax : type variableName = value;
+    String myName = "Hassan";
+    System.out.println(myName);
+  }
+}
