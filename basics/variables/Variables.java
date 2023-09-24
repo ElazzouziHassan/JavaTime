6976668e347af674c009a48f5bcedba4396de684
wizardy <wizardhassan99@gmail.com>
2023-09-24 11:32:27 +0100

    commit

--- basics/variables/Variables.java
+++ basics/variables/Variables.java
@@ -4,10 +4,13 @@ public class Variables {
   public static void main(String[] args) {
     // Syntax : type variableName = value;
     String myName = "Hassan";
+    String nickName = "Wizardy";
     int my_age = 0;
     my_age = 26;
     final int LuckyNumber = 7;
-    System.out.println(myName);
-    System.out.println(my_age);
+    float FloatNum = 5.99f;
+    char oneLetter = 'D';
+    boolean bool = true;
+    System.out.println("Hello, It's me "+myName+ " & People call me "+ nickName);
   }
 }
