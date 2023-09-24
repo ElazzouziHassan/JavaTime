207dd81115268050922a984be06403668f9bc686
wizardy <wizardhassan99@gmail.com>
2023-09-24 11:35:08 +0100

    concatination

--- basics/variables/Variables.java
+++ basics/variables/Variables.java
@@ -3,14 +3,23 @@ package variables;
 public class Variables {
   public static void main(String[] args) {
     // Syntax : type variableName = value;
-    String myName = "Hassan";
-    String nickName = "Wizardy";
-    int my_age = 0;
-    my_age = 26;
-    final int LuckyNumber = 7;
-    float FloatNum = 5.99f;
-    char oneLetter = 'D';
-    boolean bool = true;
-    System.out.println("Hello, It's me "+myName+ " & People call me "+ nickName);
+    // String myName = "Hassan";
+    // String nickName = "Wizardy";
+    // int my_age = 0;
+    // my_age = 26;
+    // final int LuckyNumber = 7;
+    // float FloatNum = 5.99f;
+    // char oneLetter = 'D';
+    // boolean bool = true;
+    // System.out.println("Hello, It's me "+myName+ " & People call me "+ nickName);
+    /*
+     * concatination
+     */
+    String FullName = "";
+    String firstName = "Hassan";
+    String lastName = "El azzouzi";
+    FullName = firstName + ' ' + lastName;
+    System.out.println(FullName);
+
   }
 }
